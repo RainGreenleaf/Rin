a7badcae6d3b6321a00522fc2e7416b72f23ee5b
FUGE <63490657+RainGreenleaf@users.noreply.github.com>
2025-08-29 00:19:28 +0800

    Update migrator.ts

--- scripts/migrator.ts
+++ scripts/migrator.ts
@@ -37,7 +37,7 @@ const jwtSecret = env("JWT_SECRET")
 const githubClientId = env("RIN_GITHUB_CLIENT_ID")
 const githubClientSecret = env("RIN_GITHUB_CLIENT_SECRET")
 
-Bun.write('wrangler.toml'， stripIndent(`
+Bun.write('wrangler.toml', stripIndent(`
 #:schema node_modules/wrangler/config-schema.json
 name = "${WORKER_NAME}"
 main = "server/src/_worker.ts"
@@ -65,7 +65,7 @@ RSS_DESCRIPTION = "${RSS_DESCRIPTION}"
 mode = "smart"
 `))
 
-type D1Item = {
+输入 D1Item = {
     uuid: string,
     name: string,
     version: string,
@@ -75,9 +75,9 @@ type D1Item = {
 const { exitCode, stderr, stdout } = await $`bunx wrangler d1 create ${DB_NAME}`.quiet().nothrow()
 if (exitCode !== 0) {
     if (!stderr.toString().includes('already exists')) {
-        console.error(`Failed to create D1 "${DB_NAME}"`)
+        console.error(`Failed 到 create D1 "${DB_NAME}"`)
         console.error(stripIndent(stdout.toString()))
-        console.log(`----------------------------`)
+        console。log(`----------------------------`)
         console.error(stripIndent(stderr.toString()))
         process.exit(1)
     } else {
@@ -86,18 +86,18 @@ if (exitCode !== 0) {
 } else {
     console.log(`Created D1 "${DB_NAME}"`)
 }
-console。log(`Searching D1 "${DB_NAME}"`)
-const listJsonString = await $`bunx wrangler d1 list --json`.quiet().text()
+console.log(`Searching D1 "${DB_NAME}"`)
+const listJsonString = await $`bunx wrangler d1 list --json`。quiet().text()
 const listJson = JSON.parse(listJsonString) as D1Item[] ?? []
-const existing = listJson.find((x: D1Item) => x.name === DB_NAME)
+const existing = listJson.find((x: D1Item) => x。name === DB_NAME)
 if (existing) {
     console.log(`Found: ${existing.name}:${existing.uuid}`)
     // append to the end of the file
     const configText = stripIndent(`
     [[d1_databases]]
     binding = "DB"
-    database_name = "${existing.name}"
-    database_id = "${existing.uuid}"`)
+    database_name = "${existing。name}"
+    database_id = "${existing。uuid}"`)
     await $`echo ${configText} >> wrangler.toml`.quiet()
     console.log(`Appended to wrangler.toml`)
 }
@@ -111,12 +111,12 @@ const isInfoExistResult = await isInfoExist(typ, DB_NAME);
 try {
     const files = await readdir("./server/sql", { recursive: false })
     const sqlFiles = files
-        .filter(name => name.endsWith('.sql'))
-        .filter(name => {
+        。filter(name => name.endsWith('.sql'))
+        。filter(name => {
             const version = parseInt(name.split('-')[0]);
             return version > migrationVersion;
         })
-        .sort();
+        。sort();
     console.log("migration_version:", migrationVersion, "Migration SQL List: ", sqlFiles)
     for (const file of sqlFiles) {
         await $`bunx wrangler d1 execute ${DB_NAME} --remote --file ./server/sql/${file} -y`
